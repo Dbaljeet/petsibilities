10149ec698c542a7f6c04a9e654bffaeb92887d1
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2022-09-19 21:15:47 -0300

    [Fix] home page, [Add]content

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <>
       <Html>
         <Head>
           <link
-            href='https://fonts.googleapis.com/css2?family=Open+Sans'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&display=optional"
+            rel="stylesheet"
           />
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,12 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.HomeBar}>
           <Image
+            alt="Pestisibilities logo"
             className={styles.image}
             src="/Logo.png"
             width={200}
             height={100}
+            layout="fixed"
           />
         </div>
         <section className={styles.Content}>
@@ -42,7 +44,62 @@ export default function Home() {
           <div className={styles.ContainerImageDog}></div>
         </section>
         <section className={styles.section2}>
-          <h3>Proponemos grandes soluciones</h3>
+          <h2>Disponible en todo Chile</h2>
+          <h2>Proponemos grandes soluciones</h2>
+          <p className={styles.p}>
+            Tanto para los que adoptan como para quienes ponen en adopción
+            otorgamos y disponemos de :
+          </p>
+          <div className={styles.section2_ul}>
+            <li className={styles.section2_ul_li}>
+              <Image
+                alt="punto 1, visibilidad para cada mascota"
+                className={styles.li_image}
+                src="/pexels-alex-andrews1.jpg"
+                width={4700}
+                height={7042}
+                layout="intrinsic"
+              />
+              <label className={styles.li_label}>
+                <p className={styles.li_label1}>1</p>
+                <p className={styles.li_label2}>
+                  Visibilidad para cada mascota
+                </p>
+              </label>
+            </li>
+            <li className={styles.section2_ul_li}>
+              <Image
+                alt="punto 2, Valoración entre perfiles"
+                className={styles.li_image}
+                src="/pexels-dominika-roseclay.jpg"
+                width={4700}
+                height={7042}
+                layout="intrinsic"
+              />
+              <label className={styles.li_label}>
+                <p className={styles.li_label1}>2</p>
+                <p className={styles.li_label2}>Valoración entre perfiles</p>
+              </label>
+            </li>
+            <li className={styles.section2_ul_li}>
+              <Image
+                alt="punto 3, Condiciones de adopción"
+                className={styles.li_image}
+                src="/pexels-nataliya-vaitkevich.jpg"
+                width={4700}
+                height={7042}
+                layout="intrinsic"
+              />
+              <label className={styles.li_label}>
+                <p className={styles.li_label1}>3</p>
+                <p className={styles.li_label2}>Condiciones de adopción</p>
+              </label>
+            </li>
+            <div>entre otras</div>
+          </div>
+
+          <p className={styles.p}>Entregamos un espacio seguro para todos</p>
+          <Link href="">Más información sobre cómo funciona Petsibilities</Link>
         </section>
       </main>
     </div>
