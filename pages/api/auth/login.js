9817bc3b5a9fb5070fbff150fc46466a7e731fa7
jose-2307 <joseluisflores2307@gmail.com>
2022-10-02 18:25:44 -0300

    changed login

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,4 +1,20 @@
+const ENDPOINT = "http://localhost:3000/api/v1";
+
 export default async function (req, res) {
   const { email, password } = req.body
-  console.log(email)
+  return fetch(`${ENDPOINT}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error('error response')
+      return res.json()
+    })
+    .then((res) => {
+      const { token } = res
+      return token
+    })
 }
