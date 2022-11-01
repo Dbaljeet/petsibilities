f9e7cc8ba46ec7c5901ccb5cb65684af38d8717f
Paint-69 <alberto.milla@alumnos.ucn.cl>
2022-11-01 03:13:04 -0300

    Create user_profile.js

---
+++ pages/user_profile.js
@@ -0,0 +1,20 @@
+import {
+  Box,
+  Grid,
+  Typography,
+  CardMedia,
+  Card,
+  CardContent,
+} from '@mui/material'
+import { UserLayout } from '../components/layouts'
+
+export default function user_profile(){
+  return (
+    <>
+      <UserLayout>
+
+        
+      </UserLayout>
+    </>
+  )
+}
