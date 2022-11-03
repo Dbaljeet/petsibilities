48b6c6b9ead46d75642a3d7d19858dba53a73283
Paint-69 <alberto.milla@alumnos.ucn.cl>
2022-11-02 23:27:19 -0300

    Update user_profile.js

--- pages/user_profile.js
+++ pages/user_profile.js
@@ -1,19 +1,26 @@
-import {
-  Box,
-  Grid,
-  Typography,
-  CardMedia,
-  Card,
-  CardContent,
-} from '@mui/material'
+import { initialData } from '../database/pets'
+import {Box,Grid,Typography,CardMedia,Card,CardContent,} from '@mui/material'
 import { UserLayout } from '../components/layouts'
+
+const pet = initialData.pets[0]
 
 export default function user_profile(){
   return (
     <>
-      <UserLayout>
-
-        
+      <UserLayout
+        title={`Profile User ~ Petsibilities`}
+        pageDescription={`Tu perfil de usuario`}
+      >
+        <Grid container spacing = {8} sx={{ marginTop: '10px' }} justifyContent = "center">
+          <Grid item xs={12} sm={8} display="flex" flexDirection="column" alignItems="center" justifyContent="center" sx={{
+              gap: '30px',
+              padding: '10px',
+            }}>
+          <Typography variant = "h2" fontWeight={500} textAlign="center">
+            {pet.name}
+          </Typography>
+          </Grid>
+        </Grid>
       </UserLayout>
     </>
   )
